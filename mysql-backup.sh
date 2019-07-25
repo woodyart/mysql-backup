08b8465ebc311b4422eded6f5ad79d58dfad22ae
Andrew Dolbilin <woodyart666@gmail.com>
2019-07-25 14:24:59 +0300

    Add arguments

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 
-MYSQL_DEFAULTS=$1
-SAVEDIR=$2
-DAYSOLD=$3
+function help() {
+  echo "
+Use script with next options:
+
+OPTIONS
+  -d or --defaults config_file
+          Specify mysql defaults file with credentials
+
+  -s or --savedir path
+          Path to store backups
+
+  -k or --keepdays number
+          Days to store backups
+
+EXAPMLE
+  $0 -d /etc/mysql/debian.cnf -s /home/backups -k 30"
+}
+
+if [[ "$#" -eq 0 ]]; then
+  echo "ERROR
+  Parameters not set"; help
+  exit 1
+fi
+
+while [[ "$#" -gt 0 ]]; do case $1 in
+  -d|--defaults) MYSQL_DEFAULTS="$2"; shift;;
+  -s|--savedir) SAVEDIR="$2"; shift;;
+  -k|--keepdays) DAYSOLD="$2"; shift;;
+  *) echo "ERROR
+  Unknown parameter passed: $1"; help; exit 1;;
+esac; shift; done
+
+if [[ -z $MYSQL_DEFAULTS || -z $SAVEDIR || -z $DAYSOLD ]]; then
+  echo "ERROR
+  Parameters not set completely"; help
+  exit 1
+fi
 
 TIMESTAMP=$(date "+%Y%m%d_%H%M%S")
 EXCLUDES="mysql\|information_schema\|performance_schema"
